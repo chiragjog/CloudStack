097c156c9ec93376c2da5eaf8a6c28f34ccf5281
Mayur Dhande <mayur.dhande@clogeny.com>
2012-04-16 20:49:42 +0530

    Removed db.commit() - No longer needed as connection is open/closed every time

--- tools/testClient/dbConnection.py
+++ tools/testClient/dbConnection.py
@@ -10,68 +10,50 @@ class dbConnection(object):
         self.user = user
         self.passwd = passwd
         self.database = db
-        
-        try:
-            self.db = pymysql.Connect(host=host, port=port, user=user, passwd=passwd, db=db)
-        except:
-            traceback.print_exc()
-            raise cloudstackException.InvalidParameterException(sys.exc_info())
- 
+
     def open(self):
         try:
             self.db = pymysql.Connect(
-					host=self.host, 
-					port=self.port, 
-					user=self.user, 
-					passwd=self.passwd, 
+					host=self.host,
+					port=self.port,
+					user=self.user,
+					passwd=self.passwd,
 					db=self.database
 				)
         except:
             traceback.print_exc()
             raise cloudstackException.InvalidParameterException(sys.exc_info())
-    
+
     def __copy__(self):
         return dbConnection(self.host, self.port, self.user, self.passwd, self.database)
-    
+
     def close(self):
         try:
             self.db.close()
         except:
             pass
-    
+
     def execute(self, sql=None):
         if sql is None:
             return None
-        
+
         resultRow = []
         cursor = None
         try:
 
 	    # Open the database connection
 	    self.open()
-            # The cloudstackConnection object provides a database connection to execute
-            # queries from the test cases. While running multiple tests, the first
-            # database query returns valid data, while the rest of the queries in other
-            # test cases returns empty lists. The reason being that  probably after querying
-            # an InnoDB table, the Cloudstack server inserts new data in the meantime. If
-            # that is the case, the MySQL sever automatically starts a new transaction for
-            # the already open connection, and since we don't call dbConnection.commit() or
-            # .rollback()anywhere, that connection is forever stuck in that transaction. InnoDB's
-            # default settings make sure that whenever we query data, we'll always see the same
-            # result within one transaction. So whatever some other process is inserting into
-            # the table is hidden from this thread's  connection.
-            self.db.commit()
 
 	    cursor = self.db.cursor()
             cursor.execute(sql)
-        
+
             result = cursor.fetchall()
             if result is not None:
                 for r in result:
                     resultRow.append(r)
             return resultRow
         except pymysql.MySQLError, e:
-            raise cloudstackException.dbException("db Exception:%s"%e) 
+            raise cloudstackException.dbException("db Exception:%s"%e)
         except:
             raise cloudstackException.internalError(sys.exc_info())
         finally:
@@ -80,22 +62,21 @@ class dbConnection(object):
 	    # Close the database connection
 	    self.close()
 
-        
     def executeSqlFromFile(self, fileName=None):
         if fileName is None:
             raise cloudstackException.InvalidParameterException("file can't not none")
-        
+
         if not os.path.exists(fileName):
             raise cloudstackException.InvalidParameterException("%s not exists"%fileName)
-        
+
         sqls = open(fileName, "r").read()
         return self.execute(sqls)
-    
+
 if __name__ == "__main__":
     db = dbConnection()
     '''
     try:
-     
+
         result = db.executeSqlFromFile("/tmp/server-setup.sql")
         if result is not None:
             for r in result:
