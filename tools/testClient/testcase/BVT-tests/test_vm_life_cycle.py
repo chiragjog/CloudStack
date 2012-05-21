47ac5f2c1e97f3a8690d7234320f72a2d2a9a161
Mayur Dhande <mayur.dhande@clogeny.com>
2012-05-21 23:52:19 +0530

    Skipped additional test in VM life cycle

--- tools/testClient/testcase/BVT-tests/test_vm_life_cycle.py
+++ tools/testClient/testcase/BVT-tests/test_vm_life_cycle.py
@@ -190,20 +190,20 @@ class TestDeployVM(cloudstackTestCase):
                 "Verify listVirtualMachines response for virtual machine: %s" \
                 % self.virtual_machine.id
             )
-        
+
         self.assertEqual(
                             isinstance(list_vm_response, list),
                             True,
                             "Check list response returns a valid list"
                         )
-        
+
         self.assertNotEqual(
                             len(list_vm_response),
                             0,
                             "Check VM available in List Virtual Machines"
                         )
         vm_response = list_vm_response[0]
-        
+
         self.assertEqual(
 
                             vm_response.id,
@@ -327,7 +327,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                                             self.apiclient,
                                             id=self.small_virtual_machine.id
                                             )
-        
+
         self.assertEqual(
                             isinstance(list_vm_response, list),
                             True,
@@ -352,7 +352,7 @@ class TestVMLifeCycle(cloudstackTestCase):
         # Validate the following
         # 1. listVM command should return this VM.State
         #    of this VM should be Running".
-        
+
         self.debug("Starting VM - ID: %s" % self.virtual_machine.id)
         self.small_virtual_machine.start(self.apiclient)
 
@@ -365,7 +365,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                             True,
                             "Check list response returns a valid list"
                         )
-        
+
         self.assertNotEqual(
                             len(list_vm_response),
                             0,
@@ -404,7 +404,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                             True,
                             "Check list response returns a valid list"
                         )
-        
+
         self.assertNotEqual(
                             len(list_vm_response),
                             0,
@@ -427,39 +427,39 @@ class TestVMLifeCycle(cloudstackTestCase):
         #    this Vm matches the one specified for "Small" service offering. 
         # 2. Using  listVM command verify that this Vm 
         #    has Small service offering Id.
-        
+
         self.debug("Stopping VM - ID: %s" % self.medium_virtual_machine.id)
-        
+
         self.medium_virtual_machine.stop(self.apiclient)
-        
+
         # Poll listVM to ensure VM is stopped properly
         timeout = self.services["timeout"]
-        
+
         while True:
             time.sleep(self.services["sleep"])
-        
+
             # Ensure that VM is in stopped state
             list_vm_response = list_virtual_machines(
                                             self.apiclient,
                                             id=self.medium_virtual_machine.id
                                             )
-            
+
             if isinstance(list_vm_response, list):
-                
+
                 vm = list_vm_response[0]
                 if vm.state == 'Stopped':
                     self.debug("VM state: %s" % vm.state)
                     break
-            
-            if timeout == 0: 
+
+            if timeout == 0:
                     raise Exception(
                         "Failed to stop VM (ID: %s) in change service offering" % vm.id)
-                    
+
             timeout = timeout - 1
-        
-        self.debug("Change Service offering VM - ID: %s" % 
+
+        self.debug("Change Service offering VM - ID: %s" %
                                     self.medium_virtual_machine.id)
-        
+
         cmd = changeServiceForVirtualMachine.changeServiceForVirtualMachineCmd()
         cmd.id = self.medium_virtual_machine.id
         cmd.serviceofferingid = self.small_offering.id
@@ -467,32 +467,32 @@ class TestVMLifeCycle(cloudstackTestCase):
 
         self.debug("Starting VM - ID: %s" % self.medium_virtual_machine.id)
         self.medium_virtual_machine.start(self.apiclient)
-        
+
         # Poll listVM to ensure VM is started properly
         timeout = self.services["timeout"]
-        
+
         while True:
             time.sleep(self.services["sleep"])
-        
+
             # Ensure that VM is in running state
             list_vm_response = list_virtual_machines(
                                             self.apiclient,
                                             id=self.medium_virtual_machine.id
                                             )
-            
+
             if isinstance(list_vm_response, list):
-                
+
                 vm = list_vm_response[0]
                 if vm.state == 'Running':
                     self.debug("VM state: %s" % vm.state)
                     break
-            
-            if timeout == 0: 
+
+            if timeout == 0:
                     raise Exception(
                         "Failed to start VM (ID: %s) after changing service offering" % vm.id)
-                    
+
             timeout = timeout - 1
-        
+
         try:
             ssh = self.medium_virtual_machine.get_ssh_client()
         except Exception as e:
@@ -510,7 +510,7 @@ class TestVMLifeCycle(cloudstackTestCase):
         meminfo = ssh.execute("cat /proc/meminfo")
         #MemTotal:        1017464 kB
         total_mem = [i for i in meminfo if "MemTotal" in i][0].split()[1]
-        
+
         self.debug(
             "CPU count: %s, CPU Speed: %s, Mem Info: %s" % (
                                                             cpu_cnt,
@@ -543,76 +543,76 @@ class TestVMLifeCycle(cloudstackTestCase):
         #    this Vm matches the one specified for "Medium" service offering. 
         # 2. Using  listVM command verify that this Vm 
         #    has Medium service offering Id.
-        
+
         self.debug("Stopping VM - ID: %s" % self.small_virtual_machine.id)
         self.small_virtual_machine.stop(self.apiclient)
-        
+
         # Poll listVM to ensure VM is stopped properly
         timeout = self.services["timeout"]
-        
+
         while True:
             time.sleep(self.services["sleep"])
-        
+
             # Ensure that VM is in stopped state
             list_vm_response = list_virtual_machines(
                                             self.apiclient,
                                             id=self.small_virtual_machine.id
                                             )
-            
+
             if isinstance(list_vm_response, list):
-                
+
                 vm = list_vm_response[0]
                 if vm.state == 'Stopped':
                     self.debug("VM state: %s" % vm.state)
                     break
-            
-            if timeout == 0: 
+
+            if timeout == 0:
                     raise Exception(
                         "Failed to stop VM (ID: %s) in change service offering" % vm.id)
-                    
+
             timeout = timeout - 1
-        
-        self.debug("Change service offering VM - ID: %s" % 
+
+        self.debug("Change service offering VM - ID: %s" %
                                             self.small_virtual_machine.id)
-        
+
         cmd = changeServiceForVirtualMachine.changeServiceForVirtualMachineCmd()
         cmd.id = self.small_virtual_machine.id
         cmd.serviceofferingid = self.medium_offering.id
         self.apiclient.changeServiceForVirtualMachine(cmd)
-        
+
         self.debug("Starting VM - ID: %s" % self.small_virtual_machine.id)
         self.small_virtual_machine.start(self.apiclient)
-        
+
         # Poll listVM to ensure VM is started properly
         timeout = self.services["timeout"]
-        
+
         while True:
             time.sleep(self.services["sleep"])
-        
+
             # Ensure that VM is in running state
             list_vm_response = list_virtual_machines(
                                             self.apiclient,
                                             id=self.small_virtual_machine.id
                                             )
-            
+
             if isinstance(list_vm_response, list):
-                
+
                 vm = list_vm_response[0]
                 if vm.state == 'Running':
                     self.debug("VM state: %s" % vm.state)
                     break
-            
-            if timeout == 0: 
+
+            if timeout == 0:
                     raise Exception(
                         "Failed to start VM (ID: %s) after changing service offering" % vm.id)
-                    
+
             timeout = timeout - 1
-        
+
         list_vm_response = list_virtual_machines(
                                             self.apiclient,
                                             id=self.small_virtual_machine.id
                                             )
-        
+
         try:
             ssh_client = self.small_virtual_machine.get_ssh_client()
         except Exception as e:
@@ -620,7 +620,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                     "SSH Access failed for %s: %s" % \
                     (self.small_virtual_machine.ipaddress, e)
                     )
-        
+
         cpuinfo = ssh_client.execute("cat /proc/cpuinfo")
 
         cpu_cnt = len([i for i in cpuinfo if "processor" in i])
@@ -630,7 +630,7 @@ class TestVMLifeCycle(cloudstackTestCase):
         meminfo = ssh_client.execute("cat /proc/meminfo")
         #MemTotal:        1017464 kB
         total_mem = [i for i in meminfo if "MemTotal" in i][0].split()[1]
-        
+
         self.debug(
             "CPU count: %s, CPU Speed: %s, Mem Info: %s" % (
                                                             cpu_cnt,
@@ -648,7 +648,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                             self.medium_offering.cpuspeed,
                             "Check CPU Speed for medium offering"
                         )
-    
+
         self.assertAlmostEqual(
                             int(total_mem) / 1024, # In MBs
                             self.medium_offering.memory,
@@ -664,7 +664,7 @@ class TestVMLifeCycle(cloudstackTestCase):
         # 1. Should not be able to login to the VM.
         # 2. listVM command should return this VM.State
         #    of this VM should be "Destroyed".
-        
+
         self.debug("Destroy VM - ID: %s" % self.small_virtual_machine.id)
         self.small_virtual_machine.delete(self.apiclient)
 
@@ -677,7 +677,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                             True,
                             "Check list response returns a valid list"
                         )
-        
+
         self.assertNotEqual(
                             len(list_vm_response),
                             0,
@@ -699,9 +699,9 @@ class TestVMLifeCycle(cloudstackTestCase):
         # 1. listVM command should return this VM.
         #    State of this VM should be "Stopped".
         # 2. We should be able to Start this VM successfully.
-        
+
         self.debug("Recovering VM - ID: %s" % self.small_virtual_machine.id)
-        
+
         cmd = recoverVirtualMachine.recoverVirtualMachineCmd()
         cmd.id = self.small_virtual_machine.id
         self.apiclient.recoverVirtualMachine(cmd)
@@ -715,7 +715,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                             True,
                             "Check list response returns a valid list"
                         )
-        
+
         self.assertNotEqual(
                             len(list_vm_response),
                             0,
@@ -738,21 +738,21 @@ class TestVMLifeCycle(cloudstackTestCase):
         # 2. listVM command should return this VM.State of this VM
         #    should be "Running" and the host should be the host 
         #    to which the VM was migrated to
-        
+
         hosts = Host.list(
-                          self.apiclient, 
+                          self.apiclient,
                           zoneid=self.medium_virtual_machine.zoneid,
                           type='Routing'
                           )
-        
+
         self.assertEqual(
-                         isinstance(hosts, list), 
-                         True, 
+                         isinstance(hosts, list),
+                         True,
                          "Check the number of hosts in the zone"
                          )
         self.assertEqual(
-                len(hosts), 
-                2, 
+                len(hosts),
+                2,
                 "Atleast 2 hosts should be present in a zone for VM migration"
                 )
 
@@ -761,12 +761,12 @@ class TestVMLifeCycle(cloudstackTestCase):
             host = hosts[1]
         else:
             host = hosts[0]
-        
+
         self.debug("Migrating VM-ID: %s to Host: %s" % (
                                         self.medium_virtual_machine.id,
                                         host.id
                                         ))
-        
+
         cmd = migrateVirtualMachine.migrateVirtualMachineCmd()
         cmd.hostid = host.id
         cmd.virtualmachineid = self.medium_virtual_machine.id
@@ -781,7 +781,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                             True,
                             "Check list response returns a valid list"
                         )
-        
+
         self.assertNotEqual(
                             list_vm_response,
                             None,
@@ -808,9 +808,9 @@ class TestVMLifeCycle(cloudstackTestCase):
         """
         # Validate the following
         # 1. listVM command should NOT  return this VM any more.
-        
+
         self.debug("Expunge VM-ID: %s" % self.small_virtual_machine.id)
-        
+
         cmd = destroyVirtualMachine.destroyVirtualMachineCmd()
         cmd.id = self.small_virtual_machine.id
         self.apiclient.destroyVirtualMachine(cmd)
@@ -852,14 +852,14 @@ class TestVMLifeCycle(cloudstackTestCase):
                          account=self.account.account.name,
                          domainid=self.account.account.domainid
                          )
-        
+
         self.debug("Successfully created ISO with ID: %s" % iso.id)
         try:
             iso.download(self.apiclient)
         except Exception as e:
             self.fail("Exception while downloading ISO %s: %s"\
                       % (iso.id, e))
-        
+
         self.debug("Attach ISO with ID: %s to VM ID: %s" % (
                                                     iso.id,
                                                     self.virtual_machine.id
@@ -869,10 +869,10 @@ class TestVMLifeCycle(cloudstackTestCase):
         cmd.id = iso.id
         cmd.virtualmachineid = self.virtual_machine.id
         self.apiclient.attachIso(cmd)
-        
+
         try:
             ssh_client = self.virtual_machine.get_ssh_client()
-            
+
             cmds = [
                         "mkdir -p %s" % self.services["mount_dir"],
                         "mount -rt iso9660 %s %s" \
@@ -881,7 +881,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                                        self.services["mount_dir"]
                                        ),
                 ]
-        
+
             for c in cmds:
                 res = ssh_client.execute(c)
 
@@ -892,9 +892,9 @@ class TestVMLifeCycle(cloudstackTestCase):
                 #Disk /dev/xvdd: 4393 MB, 4393723904 bytes
 
         except Exception as e:
-            self.fail("SSH failed for virtual machine: %s - %s" % 
+            self.fail("SSH failed for virtual machine: %s - %s" %
                                 (self.virtual_machine.ipaddress, e))
-            
+
         # Res may contain more than one strings depending on environment
         # Split strings to form new list which is used for assertion on ISO size 
         result = []
@@ -923,23 +923,23 @@ class TestVMLifeCycle(cloudstackTestCase):
             #Unmount ISO
             command = "umount %s" % self.services["mount_dir"]
             ssh_client.execute(command)
-        
+
         except Exception as e:
-            self.fail("SSH failed for virtual machine: %s - %s" % 
+            self.fail("SSH failed for virtual machine: %s - %s" %
                                 (self.virtual_machine.ipaddress, e))
-            
+
         #Detach from VM
         cmd = detachIso.detachIsoCmd()
         cmd.virtualmachineid = self.virtual_machine.id
         self.apiclient.detachIso(cmd)
-        
+
         try:
             res = ssh_client.execute(c)
-        
+
         except Exception as e:
-            self.fail("SSH failed for virtual machine: %s - %s" % 
+            self.fail("SSH failed for virtual machine: %s - %s" %
                                 (self.virtual_machine.ipaddress, e))
-            
+
         # Check if ISO is properly detached from VM (using fdisk)
         result = self.services["diskdevice"] in str(res)
 
@@ -950,7 +950,7 @@ class TestVMLifeCycle(cloudstackTestCase):
                          )
         return
 
-
+@unittest.skip("Additional test")
 class TestVMPasswordEnabled(cloudstackTestCase):
 
     @classmethod
@@ -1078,28 +1078,28 @@ class TestVMPasswordEnabled(cloudstackTestCase):
 
     def test_11_get_vm_password(self):
         """Test get VM password for password enabled template"""
-        
+
         # Validate the following
         # 1. Create an account
         # 2. Deploy VM with default service offering and "password enabled"
         #    template. Vm should be in running state.
         # 3. Stop VM deployed in step 2
         # 4. Reset VM password. SSH with new password should be successful
-        
+
         self.debug("Stopping VM: %s" % self.vm.name)
         self.vm.stop(self.apiclient)
-        
+
         # Sleep to ensure VM is stopped properly
         time.sleep(self.services["sleep"])
-        
+
         self.debug("Resetting VM password for VM: %s" % self.vm.name)
         password = self.vm.resetPassword(self.apiclient)
         self.debug("Password reset to: %s" % password)
-        
+
         self.debug("Starting VM to verify new password..")
         self.vm.start(self.apiclient)
         self.debug("VM - %s stated!" % self.vm.name)
-        
+
         vms = VirtualMachine.list(self.apiclient, id=self.vm.id, listall=True)
         self.assertEqual(
             isinstance(vms, list),
@@ -1107,7 +1107,7 @@ class TestVMPasswordEnabled(cloudstackTestCase):
             "List VMs should retun valid response for VM: %s" % self.vm.name
             )
         virtual_machine = vms[0]
-        
+
         self.assertEqual(
                          virtual_machine.state,
                          "Running",
